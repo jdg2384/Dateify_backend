106ae805e0009d0bb5c1fc4c0e12202454270c57
Patrick Glynn <pjglynn57@gmail.com>
2018-03-14 10:54:32 -0600

    linted

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express =require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const knex = require('./knex')
+const express =require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const knex = require('./knex');
 const path = require('path');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 // Headers Cors
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PATCH,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,PATCH,PUT');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
@@ -23,88 +23,87 @@ app.use(function(req, res, next) {
 // Get All Route
 app.get('/users', (req, res, next) => {
     knex('users')
-    .then(data=>{
+    .then(data => {
         res.status(200).send(data)
-    })
-})
+    });
+});
 
 // Get One user
-app.get('/users/:id',(req,res,next) => {
-    let id = req.params.id;
+app.get('/users/:id', (req, res, next) => {
+    const id = req.params.id;
     knex('users')
-    .where('id',id)
-    .select('id','spotify_id','name','age','gender','photo','age_range',
-    'radius','desired_gender')
+    .where('id', id)
+    .select('id', 'spotify_id', 'name', 'age', 'gender', 'photo', 'age_range',
+    'radius', 'desired_gender')
     .then(data => {
-        res.send(data[0])
+        res.send(data[0]);
     })
     .catch(err => {
-        res.status(404).send(err)
-    })
-})
+        res.status(404).send(err);
+    });
+});
 
 // Post New User **WORKING**
-app.post('/users', function(req, res, next){
+app.post('/users', (req, res, next) => {
     knex('users').insert({
         name: req.body.name, // From Spotify
         spotify_id: req.body.spotify_id,
-        age: req.body.age, 
-        gender: req.body.gender, 
-        photo: req.body.photo, 
+        age: req.body.age,
+        gender: req.body.gender,
+        photo: req.body.photo,
         age_range: req.body.age_range,
         radius: req.body.radius,
         desired_gender: req.body.desired_gender,
         top_tracks: req.body.top_tracks,
         top_artists: req.body.top_tracks,
         // thumbs_down: id+':'+req.body.thumbs_down,
-    },'*') 
-    .then(user=>{
-        res.status(204).send({id:user[0].id})
+    }, '*')
+    .then(user => {
+        res.status(204).send({ id: user[0].id });
     })
     .catch(err => {
-        res.status(404).send(err)
-    })
-})
+        res.status(404).send(err);
+    });
+});
 
 // Patch User info **WORKING**
-app.patch('/users/:id',(req,res,next) => {
-    let id = req.params.id; 
+app.patch('/users/:id', ( req, res, next) => {
+    const id = req.params.id;
     knex('users')
-    .where('id',id)
+    .where('id', id)
     .update({
         spotify_id: req.body.spotify_id,
-        age: req.body.age, 
-        gender: req.body.gender, 
-        photo: req.body.photo, 
+        age: req.body.age,
+        gender: req.body.gender,
+        photo: req.body.photo,
         age_range: req.body.age_range,
         radius: req.body.radius,
         desired_gender: req.body.desired_gender,
         thumbs_down: req.body.thumbs_down,
     })
-    .then(data =>{
-        res.send(data[0])
+    .then(data => {
+        res.send(data[0]);
     })
     .catch(err => {
-        res.status(404).send(err)
-    })
-})
+        res.status(404).send(err);
+    });
+});
 
 // Delete User **WORKING**
-app.delete('/users/:id',(req,res,next) => {
-    let id = req.params.id;
-    let body = req.body;
+app.delete('/users/:id', (req, res, next) => {
+    const id = req.params.id;
     knex('users')
-    .where('id',id)
-    .returning(['id','name','spotify_id','age','gender', 'gender', 'photo',
-    'age_range','radius','desired_gender','thumbs_up','thumbs_down'])
+    .where('id', id)
+    .returning(['id', 'name', 'spotify_id', 'age', 'gender', 'gender', 'photo',
+    'age_range', 'radius', 'desired_gender', 'thumbs_up', 'thumbs_down'])
     .del()
     .then(data => {
-        res.send(data[0])
+        res.send(data[0]);
     })
     .catch(err => {
-        res.status(404).send(err)
-    })
-})
+        res.status(404).send(err);
+    });
+});
 
 
 ///////////////////////////////////////
@@ -113,128 +112,122 @@ app.delete('/users/:id',(req,res,next) => {
 // Get All Match **WORKING**
 app.get('/likes', (req, res, next) => {
     knex('likes')
-    .then(data=>{
-        res.status(200).send(data)
-    })
-})
+    .then(data => {
+        res.status(200).send(data);
+    });
+});
 
 // Get One likes **WORKING**
-app.get('/likes/:id',(req,res,next) => {
-    let id = req.params.id;
-    console.log('logging',req.params.id)
+app.get('/likes/:id', (req, res, next) => {
+    const id = req.params.id;
+    console.log('logging', req.params.id);
     knex('likes')
-    .where('id',id)
-    .select('id','user_id_one','user_id_two')
+    .where('id', id)
+    .select('id', 'user_id_one', 'user_id_two')
     .then(data => {
-        res.send(data[0])
+        res.send(data[0]);
     })
     .catch(err => {
-        res.status(404).send(err)
-    })
-})
+        res.status(404).send(err);
+    });
+});
 
 // Post All Match **WORKING**
-app.post('/likes', function(req, res, next){
-    const { user_id_one, user_id_two } = req.body
+app.post('/likes', (req, res, next) => {
+    const { user_id_one, user_id_two } = req.body;
     knex('likes').insert({
         user_id_one: user_id_one,
         user_id_two: user_id_two,
-    },'*') 
+    }, '*')
     .then(()=>{
-        return knex('likes').where({'user_id_two': user_id_one, 'user_id_one': user_id_two})
+        return knex('likes').where({ 'user_id_two': user_id_one, 'user_id_one': user_id_two })
     })
-    .then(match=>{
-        const object = {
-            match: false
+    .then(match => {
+        const object = { match: false };
+        if (match[0]) {
+            object.match = true;
+            object.matchInfo = match[0];
         }
-        if(match[0]){
-            object.match = true
-            object.matchInfo = match[0]
-        }
-        res.send(object) // Ask Teddi about status not returning true
+        res.send(object); // Ask Teddi about status not returning true
     })
     .catch(err => {
-        res.status(404).send(err)
-    })
-})
+        res.status(404).send(err);
+    });
+});
 
 // Delete likes **WORKING**
-app.delete('/likes/:id',(req,res,next) => {
-    let id = req.params.id;
-    let body = req.body;
+app.delete('/likes/:id', (req, res, next) => {
+    const id = req.params.id;
     knex('likes')
-    .where('id',id)
-    .returning(['id','user_id_one','user_id_two'])
+    .where('id', id)
+    .returning(['id', 'user_id_one', 'user_id_two'])
     .del()
     .then(data => {
-        res.send(data[0])
+        res.send(data[0]);
     })
     .catch(err => {
-        res.status(404).send(err)
-    })
-})
+        res.status(404).send(err);
+    });
+});
 
 ///////////////////////////////////////
 // ****** dislikes Table Routes ****** //
 
 app.get('/dislikes', (req, res, next) => {
     knex('dislikes')
-    .then(data=>{
-        res.status(200).send(data)
-    })
-})
+    .then(data => {
+        res.status(200).send(data);
+    });
+});
 
-app.get('/dislikes/:id',(req,res,next) => {
-    let id = req.params.id;
+app.get('/dislikes/:id', (req, res, next) => {
+    const id = req.params.id;
     knex('dislikes')
-    .where('user_id_one',id)
+    .where('user_id_one', id)
     .select('dislikesUser')
     .then(data => {
-        let newData =data.map(item => {
-            return item.dislikesUser
-        })
+        const newData = data.map(item => {
+            return item.dislikesUser;
+        });
         //console.log(newData)
-        res.send(newData)
+        res.send(newData);
     })
     .catch(err => {
         res.status(404).send(err)
-    })
-})
+    });
+});
 
-app.post('/dislikes', function(req, res, next){
-    const { user_id_one, dislikesUser } = req.body
+app.post('/dislikes', (req, res, next) => {
+    const { user_id_one, dislikesUser } = req.body;
     knex('dislikes').insert({
         user_id_one: user_id_one,
         dislikesUser: dislikesUser,
-    },'*') 
-    .then(()=>{
-        return knex('dislikes').where({'user_id_one': user_id_one})
-        .returning('dislikesUser')
+    }, '*')
+    .then(() => {
+        return knex('dislikes').where({ 'user_id_one': user_id_one })
+        .returning('dislikesUser');
     })
-    .then(match=>{
-        const object = {
-            match: false
+    .then(match => {
+        const object = { match: false };
+        if (match[0]) {
+            object.match = true;
+            object.matchInfo = match[0];
         }
-        if(match[0]){
-            object.match = true
-            object.matchInfo = match[0]
-        }
-        res.send(object) // Ask Teddi about status not returning true
+        res.send(object); // Ask Teddi about status not returning true
     })
     .catch(err => {
-        res.status(404).send(err)
-    })
-})
+        res.status(404).send(err);
+    });
+});
 
 // Error Routes
 app.use((err, req, res, next) => {
-    const status = err.status || 404
-    res.status(status).json({ error: err })
+    const status = err.status || 404;
+    res.status(status).json({ error: err });
 })
 app.use((req, res, next) => {
-    res.status(404).json({ error: { status: 404, message: 'Not found' }})
+    res.status(404).json({ error: { status: 404, message: 'Not found' } });
 })
 
-const listener = () => `Listening on port ${port}!`
-app.listen(port, listener)
- 
+const listener = () => `Listening on port ${port}!`;
+app.listen(port, listener);
