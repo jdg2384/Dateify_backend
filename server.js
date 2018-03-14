245ca7140d5e9a46fd93602125d738e257b5b880
Patrick Glynn <pjglynn57@gmail.com>
2018-03-14 10:54:49 -0600

    linted

--- server.js
+++ server.js
@@ -224,10 +224,10 @@ app.post('/dislikes', (req, res, next) => {
 app.use((err, req, res, next) => {
     const status = err.status || 404;
     res.status(status).json({ error: err });
-})
+});
 app.use((req, res, next) => {
     res.status(404).json({ error: { status: 404, message: 'Not found' } });
-})
+});
 
 const listener = () => `Listening on port ${port}!`;
 app.listen(port, listener);
